a317dd123a184d53fa5b9b241e8a18c5afd55342
zacharygilliom <zacharygilliom@gmail.com>
2021-11-08 22:09:50 -0500

    added input window and fixed list display.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-
-function getGreeting(nameList, element) {
-	for (i = 0; i < nameList.length; i++) {
-		document.getElementById(element+i).innerHTML = nameList[i]
+function getGreeting(text, element) {
+	if (text.length >1) {
+		var output = text[0] + "<BR>";
+		for (i = 1; i < text.length; i++) {
+			var output = output+ text[i] + "<BR>";
+		}
+	} else {
+		var output = text[0]
 	}
+	document.getElementById(element).innerHTML = output;
 }
 
-getGreeting(["Hello", "World!"],"heading")
+var input = window.prompt("Please Enter the Prompt you wish to Print: ");
+
+var text = input.split(" ");
+
+getGreeting(text, "heading");

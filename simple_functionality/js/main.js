be441245a7c898fca02f2c3902c326c046040f4b
zacharygilliom <zacharygilliom@gmail.com>
2021-11-14 02:37:36 -0500

    Updated output from form submit and removed static table.

--- simple_functionality/js/main.js
+++ simple_functionality/js/main.js
@@ -5,10 +5,32 @@ function carGreet(carMessage) {
 	document.getElementById("car-greet-message").innerHTML = carMessage;
 }
 
-function displayOutput(models) {
+function displayModels(models) {
+
+}
+
+function displayOptionsTable(models) {
+	/*if (document.getElementById("output-list")) {
+		document.getElementById("output-list").remove();
+	}
 	const newDiv = document.createElement("div");
+	newDiv.setAttribute("id", "output-list");
+	var tableHead = document.createElement("th");
+	*/
+	var table = document.getElementById("car-table");
+
+	function addCell(tr, text) {
+		var td = tr.insertCell();
+		td.textContext = text;
+		return td;
+	}
+	var thead = table.createTHead();
+	var headerRow = th.insertRow();
+	addCell(headerRow, 'Model');
+	addCell(headerRow, '
+
 	models.forEach((item)=>{
-		var opt = document.createTextNode(item+", ");
+		//var opt = document.createTextNode(item+", ");
 		newDiv.appendChild(opt);
 	});
 	const currentDiv = document.getElementById("car-lister");
@@ -31,7 +53,9 @@ const form = document.getElementById("submit");
 form.addEventListener('click', function(event) {
 	event.preventDefault();
 	let carBrand = document.getElementById('cars').value;
-	let models = carModels[carBrand];
-	displayOutput(models);
+	if (carBrand) { 
+		let models = carModels[carBrand];
+		displayModels(models);
+	}
 });
 
